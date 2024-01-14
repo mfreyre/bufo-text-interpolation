083c4d961d3349dd7d84f9dd9aec771c07c5f2b7
mfreyre <mariel.freyre@gmail.com>
2024-01-14 15:00:02 -0500

    refactor main

--- src/main.py
+++ src/main.py
@@ -1,23 +1,31 @@
 import os
 
-directory_path = 'bufo-images/'  # Replace with your directory path
+def get_emoji_names():
+    directory_path = 'bufo-images/'  # Replace with your directory path
 
-# Set the output file name
-output_file_name = 'emoji-names.txt'
+    # Set the output file name
+    output_file_name = 'emoji-names.txt'
 
-# List to hold file names
-file_names = []
+    # List to hold file names
+    file_names = []
 
-# Read the file names in the directory
-for file in os.listdir(directory_path):
-    if os.path.isfile(os.path.join(directory_path, file)):
-        name, _ = os.path.splitext(file)  # Split the name and extension, ignore the extension
-        file_names.append(name)
+    # Read the file names in the directory
+    for file in os.listdir(directory_path):
+        if os.path.isfile(os.path.join(directory_path, file)):
+            name, _ = os.path.splitext(file)  # Split the name and extension, ignore the extension
+            file_names.append(name)
 
-# Write the file names without extensions to the output file
-with open(output_file_name, 'w') as file:
-    for name in file_names:
-        file.write(name + '\n')
+    # Write the file names without extensions to the output file
+    with open(output_file_name, 'w') as file:
+        for name in file_names:
+            file.write(name + '\n')
 
 
-print(f"File names written to {output_file_name}")
+    print(f"File names written to {output_file_name}")
+
+
+def main():
+    get_emoji_names()
+
+if __name__ == '__main__':
+    main()
